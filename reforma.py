412bfa771af10f7e1c88d22faba834657b943ed6
EkSVaY <v.ekin@g.nsu.ru>
2024-02-25 16:24:22 +0700

    Create

---
+++ reforma.py
@@ -0,0 +1,31 @@
+# Part of case-study #2: Reforma
+# Developer: Ekin V., Tolkochokov A., Sokolov I., Gazizov A.
+#
+
+import ru_local as ru
+
+
+def main():
+    '''
+    Main function.
+    :return: None
+    '''
+
+    budget_in_year = float(input()) * 10 ** 9
+    length_of_roads = 1575.6 * 10 ** 3
+    cost_km_2023 = 55 * 10 ** 6
+    cost_km_2024 = 35 * 10 ** 6
+    worker_2023 = 550
+    worker_2024 = 1100
+    salary = 100 * 10 ** 3
+    tax = 0.13
+
+    final_year =
+    total_price =
+    km_of_workers =
+    percentage_of_salaries =
+    budget_in_year_5 =
+
+
+if __name__ == '__main__':
+    main()
