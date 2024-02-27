c579ea3861d7b378e07d69483654078300340fc7
EkSVaY <v.ekin@g.nsu.ru>
2024-02-27 23:50:14 +0700

    TochnoFinal

--- reforma.py
+++ reforma.py
@@ -22,24 +22,27 @@ def main():
     salary = 100 * 10 ** 3
     tax = 0.13
 
-    final_year = (this_year + 1) + (((length_of_roads - ((budget_in_year - (cost_fabric * 20)) / cost_km_2023))
-                * cost_km_2024) / budget_in_year)
-    total_price = (budget_in_year - (cost_fabric * 20)) + (length_of_roads -
-                  (budget_in_year - (cost_fabric * 20)) / cost_km_2023) * cost_km_2024
+    final_year = (this_year + 1) + (((length_of_roads - ((budget_in_year - (cost_fabric * 20))
+                                                         / cost_km_2023)) * cost_km_2024) / budget_in_year)
+
+    total_price = ((budget_in_year - (cost_fabric * 20)) +
+                   (length_of_roads - (budget_in_year - (cost_fabric * 20)) / cost_km_2023) * cost_km_2024)
+
     km_of_workers = ((((worker_2023 * salary * 12) * tax) / cost_km_2023)
-                    + (((worker_2024 * salary * 12) * (final_year - 2023) * tax) / cost_km_2024))
-    percentage_of_salaries = ((worker_2023 * salary * tax * 12)
-                              / budget_in_year + (worker_2024 * salary * tax * (final_year - 2024) * 12)
+                     + (((worker_2024 * salary * 12) * (final_year - 2023) * tax) / cost_km_2024))
+
+    percentage_of_salaries = ((worker_2023 * salary * (1 - tax) * 12) / budget_in_year
+                              + (worker_2024 * salary * (1 - tax) * (final_year - 2024) * 12)
                               / (budget_in_year * (final_year - 2024)))
-    budget_in_year_5 = total_price / 5
+
+    budget_in_year_5 = (total_price / 5) / (10 ** 9)
 
     print(f"{ru.FINAL_YEAR_BUILD} - {int(final_year)}")
     print(f"{ru.TOTAL_PRICE_BUILD} - {total_price}")
-    print(f"{ru.KM_WORKERS} - {int(km_of_workers)}")
-    print(f"{ru.PERCENT} - {int(percentage_of_salaries)}")
-    print(f"{ru.PROJECT_5} - {int(budget_in_year_5)}")
+    print(f"{ru.KM_WORKERS} - {km_of_workers}")
+    print(f"{ru.PERCENT} - {percentage_of_salaries}")
+    print(f"{ru.PROJECT_5} - {budget_in_year_5}")
 
 
 if __name__ == '__main__':
     main()
-
